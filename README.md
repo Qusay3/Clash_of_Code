dbf87cad512c312774ca8eadb2966cac2fb22329
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-05-08 15:13:17 +0300

    Add new challenge `insertion sort`

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Clash_of_Code
 
 The repo includes various tasks/chanllenges to practice data structure and algorithm in python.
- Let's have fun and practise!
+Let's have fun and practise!
---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+'''The module defines insertion sort function'''
+
+from typing import List
+
+
+def insert_sort(arr: List[int]) -> List[int]:
+    '''sort an array of integers using insertion sort
+
+    Args:
+        arr(list:int): an array of integers
+    Returns:
+        sorted array
+    '''
+    # your code should live hear
