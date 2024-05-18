c2ae38c6166795a638606302b0e8038a96f6ce6a
Qusay3 <Ahmqusay7@gmail.com>
2024-05-18 03:01:50 -0400

    insertion_sort.py modified

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,14 +1,24 @@
-'''The module defines insertion sort function'''
-
 from typing import List
 
-
 def insert_sort(arr: List[int]) -> List[int]:
-    '''sort an array of integers using insertion sort
+    '''Sort an array of integers using insertion sort
 
     Args:
-        arr(list:int): an array of integers
+        arr (list[int]): an array of integers
     Returns:
-        sorted array
+        list[int]: the sorted array
     '''
-    # your code should live hear
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i - 1
+
+        while j >= 0 and temp < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+
+    return arr
+
+mylist = [4, 2, 7, 5, 9, 4, 5, 8, 1]
+insert_sort(mylist)
+print(mylist)
